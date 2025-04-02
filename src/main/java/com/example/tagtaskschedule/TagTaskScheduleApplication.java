7160719664e19f53d9285b7f9432b5fa6e419eb5
hemsej018 <hemsej018@naver.com>
2025-04-02 19:24:26 +0900

    Initial commit

---
+++ src/main/java/com/example/tagtaskschedule/TagTaskScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tagtaskschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagTaskScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TagTaskScheduleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tagtaskschedule/TagTaskScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tagtaskschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagTaskScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
